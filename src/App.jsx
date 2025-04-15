9e542f39cf54d1ac2539666677099e35d396e80f
fadillaiizzar <fadillaiizzarm@gmail.com>
2025-04-15 23:14:54 +0700

    i don't now

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export default function App() {
+
+  return (
+    <div className="font-poppins bg-[url('/image/Luxurious%20Wallpaper.jpg')] h-screen bg-cover bg-center">
+      <div className="bg-[url('/image/Luxurious%20Wallpaper.jpg')] h-screen bg-cover bg-center flex flex-col gap-5 justify-center items-center">
+        <div className="bg-[url('/image/weed.png')] h-[600px] w-[600px] bg-cover bg-center flex flex-col gap-5 justify-center items-center">
+          <p className="mt-14">The Wedding Of</p>
+          <h1 className="text-4xl">LAILA & DAMAR</h1>
+          <div className="flex flex-col gap-2 justify-center items-center">
+            <p className="">Yth.</p>
+            <p className="">Bapak/ Ibu/ Saudara/ i</p>
+            <p className="">haha</p>
+          </div>
+          
+
+          <button className="bg-blue-950 text-white p-2 rounded-lg">goow</button>
+        </div>
+      </div>
+    </div>
+  )
+}
